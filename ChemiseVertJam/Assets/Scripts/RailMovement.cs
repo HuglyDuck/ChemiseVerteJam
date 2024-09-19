92d76281f80074a16a8bec5e5f443781910b06ee
NRexha <nikolas.rexha@gmail.com>
2024-09-19 12:30:46 +0200

    Rail Movement Bug Fix
    corrected rail movement

--- ChemiseVertJam/Assets/Scripts/RailMovement.cs
+++ ChemiseVertJam/Assets/Scripts/RailMovement.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class ObjectMovement : MonoBehaviour
 {
-    
-    public float _speed = 5f;         
-    public float _leftLimit = -5f;    
-    public float _rightLimit = 5f;    
-    public Color _gizmoColor = Color.red;
-    private float _input;   
-    private float _currentXPosition;  
-    private GameInputs _gameInput;
+    #region VARIABLES
+    [Header("Rail Parameters")]
+    [SerializeField] private float _speed = 5f;
+    [SerializeField] private float _leftLimit = -5f;
+    [SerializeField] private float _rightLimit = 5f;
+    [SerializeField] private Color _gizmoColor = Color.red;
 
+
+    private float _input;
+    private float _currentXPosition;
+    private GameInputs _gameInput;
+    #endregion
+
+    #region INPUTS
     private void Awake()
     {
         _gameInput = new GameInputs();
@@ -36,35 +41,38 @@ public class ObjectMovement : MonoBehaviour
     {
         _input = context.ReadValue<float>();
     }
+    #endregion
     void Update()
     {
-      
-        if(_input != 0)
-        {
-            UpdateMovement();
-        }
+
+
+        UpdateMovement();
+
     }
 
+    #region GIZMOS
     private void OnDrawGizmos()
     {
-        
+
         Gizmos.color = _gizmoColor;
-  
+
         Gizmos.DrawLine(new Vector3(_leftLimit, transform.position.y - 1, transform.position.z),
                         new Vector3(_leftLimit, transform.position.y + 1, transform.position.z));
-    
+
         Gizmos.DrawLine(new Vector3(_rightLimit, transform.position.y - 1, transform.position.z),
                         new Vector3(_rightLimit, transform.position.y + 1, transform.position.z));
-     
+
         Gizmos.DrawLine(new Vector3(_leftLimit, transform.position.y, transform.position.z),
                         new Vector3(_rightLimit, transform.position.y, transform.position.z));
     }
+    #endregion
 
+    #region MOVEMENT
     private void UpdateMovement()
     {
         _currentXPosition = transform.position.x;
 
-        float movement = _input  * _speed * Time.deltaTime;
+        float movement = _input * _speed * Time.deltaTime;
 
         float newPositionX = _currentXPosition + movement;
 
@@ -73,3 +81,4 @@ public class ObjectMovement : MonoBehaviour
         transform.position = new Vector3(newPositionX, transform.position.y, transform.position.z);
     }
 }
+#endregion
