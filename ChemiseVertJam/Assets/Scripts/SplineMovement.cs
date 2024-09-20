2fd4c915de3bbff09ada3a749559a841edc85384
HuglyDuck <150171589+HuglyDuck@users.noreply.github.com>
2024-09-20 17:33:48 +0200

    NewScriptStopRun

--- ChemiseVertJam/Assets/Scripts/SplineMovement.cs
+++ ChemiseVertJam/Assets/Scripts/SplineMovement.cs
@@ -2,14 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Splines;
+using UnityEngine.InputSystem;
 
 public class SplineMovement : MonoBehaviour
 {
-    public SplineContainer spline;
-    public float speed = 1f;
+    [SerializeField] private SplineContainer spline;
+    [SerializeField] private float speed = 1f;
     float distancePercentage = 0f;
 
+    [SerializeField] private float _runSpeed;
+    [SerializeField] private float _speed;
+    private float _targetSpeed;
+    private float _currentSpeed;
+    private GameInputs _inputActions;
+
     float splineLength;
+
+    private void Awake()
+    {
+        _inputActions = new GameInputs();
+    }
+
+    private void OnEnable()
+    {
+        _inputActions.InGame.StopPlayer.Enable();
+        _inputActions.InGame.RunPlayer.Enable();
+        _inputActions.InGame.RunPlayer.performed += RunPlayer_performed;
+        _inputActions.InGame.StopPlayer.performed += StopPlayer_performed;
+    }
+
+    private void RunPlayer_performed(InputAction.CallbackContext context)
+    {
+        if(_currentSpeed >= _runSpeed)
+        {
+
+        }
+    }
+
+    private void StopPlayer_performed(InputAction.CallbackContext context)
+    {
+
+    }
 
     private void Start()
     {
@@ -32,5 +65,11 @@ public class SplineMovement : MonoBehaviour
         Vector3 nextPosition = spline.EvaluatePosition(distancePercentage + 0.05f);
         Vector3 direction = nextPosition - currentPosition;
         transform.rotation = Quaternion.LookRotation(direction, transform.up);
+    }
+
+    private void OnDisable()
+    {
+        _inputActions.InGame.StopPlayer.Enable();
+        _inputActions.InGame.RunPlayer.Enable();
     }
 }
