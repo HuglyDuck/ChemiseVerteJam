068f000809c1c9e6b329310c2322017772e7f428
Metalzee <sacha.vekemanfb@gmail.com>
2024-09-21 21:01:13 +0200

    highlightColor

--- ChemiseVertJam/Assets/Scripts/InputsManager.cs
+++ ChemiseVertJam/Assets/Scripts/InputsManager.cs
@@ -36,9 +36,9 @@ public class InputsManager : MonoBehaviour
     private void OnEnable()
     {
         _inputActions.InGame.Enable();
-
         _inputActions.InGame.SelectObject.performed += SelectObject_performed;
     }
+
     private void SelectObject_performed(InputAction.CallbackContext context)
     {
         _selectValue = _inputActions.InGame.SelectObject.ReadValue<float>();
@@ -55,6 +55,7 @@ public class InputsManager : MonoBehaviour
 
     private void SelectObjectAdd()
     {
+        ResetMaterials();
         OnDesactive(_interactObjectList[_objectValue]);
         if (_objectValue >= _interactObjectList.Count - 1)
         {
@@ -66,6 +67,7 @@ public class InputsManager : MonoBehaviour
 
     private void SelectObjectSub()
     {
+        ResetMaterials();
         OnDesactive(_interactObjectList[_objectValue]);
         if (_objectValue <= 0)
         {
@@ -73,7 +75,6 @@ public class InputsManager : MonoBehaviour
         }
         else _objectValue--;
         OnActive(_interactObjectList[_objectValue]);
-
     }
 
     private void OnActive(GameObject _interactObject)
@@ -81,30 +82,44 @@ public class InputsManager : MonoBehaviour
         if (_interactObject.TryGetComponent(out ActivateSelected _scriptActivateSelected))
         {
             _scriptActivateSelected.Activate();
-            
         }
-
 
         _currentMaterial = GetObjectMaterial(_interactObject);
         _currentChildMaterial = GetChildObjectMaterial(_interactObject);
 
         int previousIndex = (_objectValue - 1 + _interactObjectList.Count) % _interactObjectList.Count;
+        int nextIndex = (_objectValue + 1) % _interactObjectList.Count;
+
         _previousMaterial = GetObjectMaterial(_interactObjectList[previousIndex]);
         _previousChildMaterial = GetChildObjectMaterial(_interactObjectList[previousIndex]);
 
-
-        int nextIndex = (_objectValue + 1) % _interactObjectList.Count;
         _nextMaterial = GetObjectMaterial(_interactObjectList[nextIndex]);
         _nextChildMaterial = GetChildObjectMaterial(_interactObjectList[nextIndex]);
 
-        
+        List<GameObject> activeObjects = new List<GameObject>
+        {
+            _interactObjectList[_objectValue],
+            _interactObjectList[previousIndex],
+            _interactObjectList[nextIndex]
+        };
+
+        foreach (GameObject obj in _interactObjectList)
+        {
+            if (!activeObjects.Contains(obj))
+            {
+                if (obj.TryGetComponent(out ActivateSelected _inactiveScript))
+                {
+                    DestroyObjectColor(obj);
+                    _inactiveScript.Desactivate();
+                }
+            }
+        }
     }
 
     private void OnDesactive(GameObject _interactObject)
     {
         if (_interactObject.TryGetComponent(out ActivateSelected _scriptActivateSelected))
         {
-            DestroyObjectColor();
             _scriptActivateSelected.Desactivate();
         }
     }
@@ -133,7 +148,6 @@ public class InputsManager : MonoBehaviour
         return null;
     }
 
-
     public void ChangeCurrentObjectColor()
     {
         if (_currentMaterial != null)
@@ -150,6 +164,7 @@ public class InputsManager : MonoBehaviour
             _currentChildMaterial.SetFloat("_MiddleValue", 1);
         }
     }
+
     public GameObject GetCurrentObject()
     {
         if (_objectValue >= 0 && _objectValue < _interactObjectList.Count)
@@ -171,7 +186,6 @@ public class InputsManager : MonoBehaviour
         return _interactObjectList[nextIndex];
     }
 
-
     public void ChangePreviousObjectColor()
     {
         if (_previousMaterial != null)
@@ -187,14 +201,31 @@ public class InputsManager : MonoBehaviour
         }
     }
 
-
-    public void DestroyObjectColor()
+    public void DestroyObjectColor(GameObject obj)
     {
-        if (_previousChildMaterial != null)
+        Material material = GetObjectMaterial(obj);
+        Material childMaterial = GetChildObjectMaterial(obj);
+
+        if (material != null)
         {
-            _previousChildMaterial.SetFloat("_LeftValue", 0);
-            _previousChildMaterial.SetFloat("_RightValue", 0);
-            _previousChildMaterial.SetFloat("_MiddleValue", 0);
+            material.SetFloat("_LeftValue", 0);
+            material.SetFloat("_RightValue", 0);
+            material.SetFloat("_MiddleValue", 0);
+        }
+
+        if (childMaterial != null)
+        {
+            childMaterial.SetFloat("_LeftValue", 0);
+            childMaterial.SetFloat("_RightValue", 0);
+            childMaterial.SetFloat("_MiddleValue", 0);
+        }
+    }
+
+    private void ResetMaterials()
+    {
+        foreach (GameObject obj in _interactObjectList)
+        {
+            DestroyObjectColor(obj);
         }
     }
 
@@ -221,7 +252,6 @@ public class InputsManager : MonoBehaviour
 
     private void Update()
     {
-        Debug.Log(_objectValue);
         ChangeCurrentObjectColor();
         ChangePreviousObjectColor();
         ChangeNextObjectColor();
