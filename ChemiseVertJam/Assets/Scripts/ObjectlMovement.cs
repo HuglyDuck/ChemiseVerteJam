a0fad9ced4a9d22090d035c0fa08b5ae8fd18999
NRexha <nikolas.rexha@gmail.com>
2024-09-23 16:31:53 +0200

    Object movement sound level2

--- ChemiseVertJam/Assets/Scripts/ObjectlMovement.cs
+++ ChemiseVertJam/Assets/Scripts/ObjectlMovement.cs
@@ -11,11 +11,15 @@ public class ObjectMovement : MonoBehaviour
     [SerializeField][Range(0, 1)] private float _spawnpoint = 0f;
     [SerializeField] private Color _gizmoColor = Color.red;
 
+    [Header("Sound Parameters")]
+    [SerializeField] private AudioSource _movementAudioSource; 
+
     private float _input;
     private GameInputs _gameInput;
     private Vector3 _movementDirection;
     private float _pathLength;
     private Vector3 _startPosition;
+    private bool _isMoving;
     #endregion
 
     #region INPUTS
@@ -30,19 +34,24 @@ public class ObjectMovement : MonoBehaviour
         _gameInput.InGame.SideMove.performed += OnSideMovement;
         _gameInput.InGame.SideMove.canceled += ResetInput;
     }
+
     private void OnDisable()
     {
         _gameInput.InGame.SideMove.Disable();
         _gameInput.InGame.SideMove.performed -= OnSideMovement;
         _gameInput.InGame.SideMove.canceled -= ResetInput;
     }
+
     public void ResetInput(InputAction.CallbackContext context)
     {
         _input = 0;
+        StopMovementSound();  
     }
+
     public void OnSideMovement(InputAction.CallbackContext context)
     {
         _input = context.ReadValue<float>();
+        if (!_isMoving) StartMovementSound(); 
     }
     #endregion
 
@@ -50,15 +59,12 @@ public class ObjectMovement : MonoBehaviour
     {
         if (_leftLimitObject && _rightLimitObject)
         {
-      
             _leftLimitObject.parent = null;
             _rightLimitObject.parent = null;
 
-          
             _movementDirection = (_rightLimitObject.position - _leftLimitObject.position).normalized;
             _pathLength = Vector3.Distance(_leftLimitObject.position, _rightLimitObject.position);
 
-          
             _startPosition = _leftLimitObject.position + _movementDirection * _spawnpoint * _pathLength;
             transform.position = _startPosition;
         }
@@ -70,8 +76,13 @@ public class ObjectMovement : MonoBehaviour
         {
             UpdateMovement();
         }
+
+   
         float targetRotationZ = Mathf.Lerp(-15f, 15f, (_input + 1f) / 2f);
         transform.rotation = Quaternion.Euler(0f, 0f, targetRotationZ);
+
+ 
+        HandleMovementSound();
     }
 
     #region GIZMOS
@@ -80,13 +91,9 @@ public class ObjectMovement : MonoBehaviour
         if (_leftLimitObject && _rightLimitObject)
         {
             Gizmos.color = _gizmoColor;
-
             Gizmos.DrawLine(_leftLimitObject.position, _rightLimitObject.position);
-
-
             Gizmos.DrawSphere(_leftLimitObject.position, 0.2f);
             Gizmos.DrawSphere(_rightLimitObject.position, 0.2f);
-
 
             if (Application.isPlaying)
             {
@@ -104,16 +111,11 @@ public class ObjectMovement : MonoBehaviour
     #region MOVEMENT
     private void UpdateMovement()
     {
-
         Vector3 currentPosition = transform.position;
         Vector3 projectedPosition = Vector3.Project(currentPosition - _leftLimitObject.position, _movementDirection) + _leftLimitObject.position;
 
-
         float movement = _input * _speed * Time.deltaTime;
-
-
         projectedPosition += _movementDirection * movement;
-
 
         float distanceFromLeft = Vector3.Distance(projectedPosition, _leftLimitObject.position);
         float distanceFromRight = Vector3.Distance(projectedPosition, _rightLimitObject.position);
@@ -128,6 +130,38 @@ public class ObjectMovement : MonoBehaviour
         }
 
         transform.position = projectedPosition;
+    }
+    #endregion
+
+    #region SOUND
+    private void StartMovementSound()
+    {
+        if (_movementAudioSource && !_movementAudioSource.isPlaying)
+        {
+            _movementAudioSource.Play();
+            _isMoving = true;
+        }
+    }
+
+    private void StopMovementSound()
+    {
+        if (_movementAudioSource && _movementAudioSource.isPlaying)
+        {
+            _movementAudioSource.Stop();
+            _isMoving = false;
+        }
+    }
+
+    private void HandleMovementSound()
+    {
+        if (_input == 0 && _isMoving)
+        {
+            StopMovementSound();
+        }
+        else if (_input != 0 && !_isMoving)
+        {
+            StartMovementSound();
+        }
     }
     #endregion
 }
